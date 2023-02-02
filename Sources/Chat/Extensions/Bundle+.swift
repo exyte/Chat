e7020a6698d8e936fc346adab01adb90124c5eea
AlisaMylnikova <alisa.mylnikova@exyte.com>
2023-02-02 14:29:16 +0700

    Fix bundle

--- Sources/Chat/Extensions/Bundle+.swift
+++ Sources/Chat/Extensions/Bundle+.swift
@@ -10,7 +10,7 @@ import Foundation
 private final class BundleToken {
     static let bundle: Bundle = {
 #if SWIFT_PACKAGE
-        return Bundle.main
+        return Bundle.module
 #else
         return Bundle(for: BundleToken.self)
 #endif
