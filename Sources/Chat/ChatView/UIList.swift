f982a88de248ca3819cb0e65465c49641368e9d2
AlisaMylnikova <alisa.mylnikova@exyte.com>
2023-05-30 13:32:49 +0700

    Fix scrollToTop crash

--- Sources/Chat/ChatView/UIList.swift
+++ Sources/Chat/ChatView/UIList.swift
@@ -49,7 +49,9 @@ struct UIList<MessageContent: View>: UIViewRepresentable {
 
         NotificationCenter.default.addObserver(forName: .onScrollToBottom, object: nil, queue: nil) { _ in
             DispatchQueue.main.async {
-                tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .bottom, animated: true)
+                if !sections.isEmpty {
+                    tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .bottom, animated: true)
+                }
             }
         }
 
