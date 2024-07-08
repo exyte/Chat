a5a746642070eb79468fd9497134ce20b16e0281
AlisaMylnikova <alisa.mylnikova@exyte.com>
2024-07-08 16:55:05 +0700

    Fix example random time

--- Example/ChatExample/Extensions/Date+Extensions.swift
+++ Example/ChatExample/Extensions/Date+Extensions.swift
@@ -6,11 +6,27 @@ import Foundation
 
 extension Date {
     func randomTime() -> Date {
-        var components = Calendar.current.dateComponents([.year, .month, .day], from: self)
-        components.hour = Int.random(min: 0, max: 23)
-        components.minute = Int.random(min: 0, max: 59)
-        components.second = Int.random(min: 0, max: 59)
+        var hour = Int.random(min: 0, max: 23)
+        var minute = Int.random(min: 0, max: 59)
+        var second = Int.random(min: 0, max: 59)
 
+        let current = Calendar.current.dateComponents([.hour, .minute, .second], from: Date())
+        let curHour = current.hour ?? 23
+        let curMinute = current.minute ?? 59
+        let curSecond = current.second ?? 59
+
+        if hour > curHour {
+            hour = curHour
+        } else if hour == curHour, minute > curMinute {
+            minute = curMinute
+        } else if hour == curHour, minute == curMinute, second > curSecond {
+            second = curSecond
+        }
+
+        var components = Calendar.current.dateComponents([.year, .month, .day], from: self)
+        components.hour = hour
+        components.minute = minute
+        components.second = second
         return Calendar.current.date(from: components)!
     }
 }
