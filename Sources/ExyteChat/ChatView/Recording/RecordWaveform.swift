751430726ecf30d06f1cee5aa4a8ecf9bef47016
gzlboy <332446332@qq.com>
2024-09-16 15:13:41 +0800

    Fix: When the recording duration is too long (for example, 2 minutes), the waveform view exceeds the screen width.

--- Sources/ExyteChat/ChatView/Recording/RecordWaveform.swift
+++ Sources/ExyteChat/ChatView/Recording/RecordWaveform.swift
@@ -12,6 +12,9 @@ struct RecordWaveformWithButtons: View {
     @Environment(\.chatTheme) private var theme
 
     @StateObject var recordPlayer = RecordingPlayer()
+    //160 is screen left-padding/right-padding and playButton's width.
+    //To ensure that the view does not exceed the screen, need to subtract
+    static let viewPadding:CGFloat = 160
 
     var recording: Recording
 
@@ -53,20 +56,29 @@ struct RecordWaveformWithButtons: View {
 }
 
 struct RecordWaveformPlaying: View {
-
+    
     var samples: [CGFloat] // 0...1
     var progress: CGFloat
     var color: Color
     var addExtraDots: Bool
-
-    var maxLength: CGFloat {
-        max((RecordWaveform.spacing + RecordWaveform.width) * CGFloat(samples.count) - RecordWaveform.spacing, 0)
+    var maxLength: CGFloat = 0.0
+    
+    private var adjustedSamples: [CGFloat] = []
+    
+    init(samples: [CGFloat], progress: CGFloat, color: Color, addExtraDots: Bool) {
+        self.samples = samples
+        self.progress = progress
+        self.color = color
+        self.addExtraDots = addExtraDots
+        self.adjustedSamples = adjustedSamples(UIScreen.main.bounds.width)
+        self.maxLength = max((RecordWaveform.spacing + RecordWaveform.width) * CGFloat(self.adjustedSamples.count) - RecordWaveform.spacing, 0)
     }
 
     var body: some View {
+        
         GeometryReader { g in
             ZStack {
-                let adjusted = adjustedSamples(g.size.width)
+                let adjusted = addExtraDots ? adjustedSamples(g.size.width) : adjustedSamples
                 RecordWaveform(samples: adjusted, addExtraDots: addExtraDots)
                     .foregroundColor(color.opacity(0.4))
                 RecordWaveform(samples: adjusted, addExtraDots: addExtraDots)
@@ -77,6 +89,7 @@ struct RecordWaveformPlaying: View {
                     }
             }
             .frame(height: RecordWaveform.maxSampleHeight)
+            
         }
         .frame(height: RecordWaveform.maxSampleHeight)
         .applyIf(!addExtraDots) {
@@ -86,27 +99,22 @@ struct RecordWaveformPlaying: View {
         .fixedSize(horizontal: !addExtraDots, vertical: true)
     }
 
-    func adjustedSamples(_ width: CGFloat) -> [CGFloat] {
-        let maxWidth = addExtraDots ? width : UIScreen.main.bounds.width
-        let maxSamples = Int(maxWidth / (RecordWaveform.width + RecordWaveform.spacing))
-
-        var adjusted = samples
-        var temp = [CGFloat]()
-        while adjusted.count > maxSamples {
-            var i = 0
-            while i < adjusted.count {
-                if i == adjusted.count - 1 {
-                    temp.append(adjusted[i])
-                    break
-                }
-
-                temp.append((adjusted[i] + adjusted[i+1])/2)
-                i+=2
-            }
-            adjusted = temp
-            temp = []
+    func adjustedSamples(_ maxWidth: CGFloat) -> [CGFloat] {
+        
+        let maxSamples = Int((maxWidth - RecordWaveformWithButtons.viewPadding) / (RecordWaveform.width + RecordWaveform.spacing))
+        let temp = samples
+        
+        if temp.count <= maxSamples {
+            return temp
         }
+        //Use ceil to ensure that the adjusted.count will not be greater than maxSamples
+        let ratio = Int(ceil( Double(temp.count) / Double(maxSamples) ))
+        let adjusted = stride(from: 0, to: temp.count, by: ratio).map {
+            temp[$0]
+        }
+        
         return adjusted
+        
     }
 }
 
@@ -126,9 +134,9 @@ struct RecordWaveform: View {
                     Capsule()
                         .frame(width: RecordWaveform.width, height: RecordWaveform.maxSampleHeight * CGFloat(s))
                 }
-
-                if addExtraDots {
-                    ForEach(samples.count..<Int(g.size.width / (RecordWaveform.width + RecordWaveform.spacing)), id: \.self) { _ in
+                let maxSampleCounts = Int((g.size.width) / (RecordWaveform.width + RecordWaveform.spacing))
+                if addExtraDots && samples.count < maxSampleCounts {
+                    ForEach(samples.count..<maxSampleCounts, id: \.self) { _ in
                         Capsule()
                             .viewSize(RecordWaveform.width)
                     }
