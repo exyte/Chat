cb28a09eb2e310baae983746846af1a9b1fcce0c
Brandon <32753167+btoms20@users.noreply.github.com>
2025-01-18 16:47:00 -0800

    Fixed `frientMessageTime` param misspelling

--- Sources/ExyteChat/ChatView/MessageView/MessageTimeView.swift
+++ Sources/ExyteChat/ChatView/MessageView/MessageTimeView.swift
@@ -13,7 +13,7 @@ struct MessageTimeView: View {
     var body: some View {
         Text(text)
             .font(.caption)
-            .foregroundColor(isCurrentUser ? chatTheme.colors.myMessageTime : chatTheme.colors.frientMessageTime)
+            .foregroundColor(isCurrentUser ? chatTheme.colors.myMessageTime : chatTheme.colors.friendMessageTime)
     }
 }
 
