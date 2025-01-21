bc9cb2b7701a53cb724c98a4a4ba2be6d6eee55c
Brandon <32753167+btoms20@users.noreply.github.com>
2025-01-21 11:48:15 -0800

    Temporary change to ContentView so that the new themeing functionality can be seen. Doesn't need to be merged!

--- Example/ChatExample/ContentView.swift
+++ Example/ChatExample/ContentView.swift
@@ -6,19 +6,38 @@ import SwiftUI
 import ExyteMediaPicker
 
 struct ContentView: View {
+    
+    @State private var isAccent:Bool = true
+    @State private var accentColor = Color(red: 83/256, green: 95/256, blue: 249/256)
+    
     var body: some View {
         NavigationView {
             List {
                 Section {
                     NavigationLink("Active chat example") {
-                        ChatExampleView(
-                            viewModel: ChatExampleViewModel(interactor: MockChatInteractor(isActive: true)),
-                            title: "Active chat example"
-                        )
+                        if !isAccent, #available(iOS 18.0, *) {
+                            ChatExampleView(
+                                viewModel: ChatExampleViewModel(interactor: MockChatInteractor(isActive: true)),
+                                title: "Active chat example"
+                            )
+                            .chatTheme(color: accentColor)
+                        } else {
+                            ChatExampleView(
+                                viewModel: ChatExampleViewModel(interactor: MockChatInteractor(isActive: true)),
+                                title: "Active chat example"
+                            )
+                            .chatTheme(accentColor: accentColor)
+                        }
                     }
                     
                     NavigationLink("Simple chat example") {
-                        ChatExampleView(title: "Simple example")
+                        if !isAccent, #available(iOS 18.0, *) {
+                            ChatExampleView(title: "Simple example")
+                                .chatTheme(color: accentColor)
+                        } else {
+                            ChatExampleView(title: "Simple example")
+                                .chatTheme(accentColor: accentColor)
+                        }
                     }
 
                     NavigationLink("Simple comments example") {
@@ -30,21 +49,36 @@ struct ContentView: View {
             }
             .navigationTitle("Chat examples")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack {
+                        if #available(iOS 18.0, *) {
+                            Button(isAccent ? "Accent" : "Themed") {
+                                isAccent.toggle()
+                            }
+                            ColorPicker("", selection: $accentColor)
+                        } else {
+                            ColorPicker("Accent", selection: $accentColor)
+                        }
+                    }
+                    
+                }
+            }
         }
         .navigationViewStyle(.stack)
         
-        .mediaPickerTheme(
-            main: .init(
-                text: .white,
-                albumSelectionBackground: .examplePickerBg,
-                fullscreenPhotoBackground: .examplePickerBg
-            ),
-            selection: .init(
-                emptyTint: .white,
-                emptyBackground: .black.opacity(0.25),
-                selectedTint: .exampleBlue,
-                fullscreenTint: .white
-            )
-        )
+//        .mediaPickerTheme(
+//            main: .init(
+//                text: .white,
+//                albumSelectionBackground: .examplePickerBg,
+//                fullscreenPhotoBackground: .examplePickerBg
+//            ),
+//            selection: .init(
+//                emptyTint: .white,
+//                emptyBackground: .black.opacity(0.25),
+//                selectedTint: .exampleBlue,
+//                fullscreenTint: .white
+//            )
+//        )
     }
 }
