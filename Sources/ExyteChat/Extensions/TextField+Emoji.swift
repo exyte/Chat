a8f1822d254d37f562ff89d2e3ab41922f29aeca
Brandon <32753167+btoms20@users.noreply.github.com>
2025-02-16 08:41:18 -0800

    Added an EmojiTextField. This view behaves similar to the standard TextFeild but when focused launches the keybaord in Emoji mode.

---
+++ Sources/ExyteChat/Extensions/TextField+Emoji.swift
@@ -0,0 +1,70 @@
+//
+//  EmojiTextField.swift
+//  Chat
+//
+//  https://stackoverflow.com/questions/66397828/emoji-keyboard-swiftui
+//
+
+import SwiftUI
+
+class UIEmojiTextField: UITextField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setEmoji() {
+        _ = self.textInputMode
+    }
+    
+    override var textInputContextIdentifier: String? {
+           return ""
+    }
+    
+    override var textInputMode: UITextInputMode? {
+        for mode in UITextInputMode.activeInputModes {
+            if mode.primaryLanguage == "emoji" {
+                self.keyboardType = .default // do not remove this
+                return mode
+            }
+        }
+        return nil
+    }
+}
+
+/// A TextField that opens the keyboard (when focused) with the emoji selector active
+struct EmojiTextField: UIViewRepresentable {
+    /// A placeholder string to display when no emoji is selected
+    var placeholder: String = ""
+    /// A binding string that the selected emoji will be assigned to
+    @Binding var text: String
+    
+    func makeUIView(context: Context) -> UIEmojiTextField {
+        let emojiTextField = UIEmojiTextField()
+        emojiTextField.placeholder = placeholder
+        emojiTextField.text = text
+        emojiTextField.delegate = context.coordinator
+        return emojiTextField
+    }
+    
+    func updateUIView(_ uiView: UIEmojiTextField, context: Context) {
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: EmojiTextField
+        
+        init(parent: EmojiTextField) {
+            self.parent = parent
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            DispatchQueue.main.async { [weak self] in
+                self?.parent.text = textField.text ?? ""
+            }
+        }
+    }
+}
