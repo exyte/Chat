7bd5d66b7a438b3bf52f550990b08ae7864cca13
samepant <samepant@users.noreply.github.com>
2025-04-24 19:17:39 -0400

    put back reverse

--- Sources/ExyteChat/Views/WrappingMessages.swift
+++ Sources/ExyteChat/Views/WrappingMessages.swift
@@ -178,6 +178,7 @@ extension ChatView {
                     positionInMessagesSection: positionInMessagesSection,
                     commentsPosition: commentsPosition)
             }
+            .reversed()
     }
 }
 
