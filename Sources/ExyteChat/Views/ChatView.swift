08470455fb9d0bbf396eb6e75379588a935d8fa5
Matthew Fennell <matthew@fennell.dev>
2025-05-31 17:12:21 +0100

    Remove unused pageSize from enableLoadMore
    Pagination offset (the distance from the top message before we start
    loading more) used to be determined by the offset parameter passed to
    enableLoadMore, like so:
    
    if ids.prefix(offset + 1).contains(message.id) {
        onEvent?(message)
    }
    
    This got changed in 568d00dc3f77324a21c3823a50797c599470cb28. Now, we
    rely on paginationTargetIndexPath instead, which is hardcoded to be the
    last already loaded message:
    
    IndexPath(row: lastSection.rows.count - 1, section: sections.count - 1)
    
    As a result, pageSize is no longer used. Remove it to make sure that
    consumers do not expect different behaviour when they modify the
    parameter.

--- Sources/ExyteChat/Views/ChatView.swift
+++ Sources/ExyteChat/Views/ChatView.swift
@@ -617,11 +617,11 @@ public extension ChatView {
         return view
     }
     
-    /// when user scrolls up to `pageSize`-th meassage, call the handler function, so user can load more messages
+    /// when user scrolls to the final message, call the handler function, to load more messages
     /// NOTE: doesn't work well with `isScrollEnabled` false
-    func enableLoadMore(pageSize: Int, _ handler: @escaping ChatPaginationClosure) -> ChatView {
+    func enableLoadMore(_ handler: @escaping ChatPaginationClosure) -> ChatView {
         var view = self
-        view.paginationHandler = PaginationHandler(handleClosure: handler, pageSize: pageSize)
+        view.paginationHandler = PaginationHandler(handleClosure: handler)
         return view
     }
     
